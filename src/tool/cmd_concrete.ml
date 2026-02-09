1bb212acee3dede00e3b656801ab10c221e135ec
Ilan <dahmaneilan.pro@gmail.com>
2026-02-09 23:04:48 +0100

    oublie du pre-commit pour formater...

--- src/tool/cmd_concrete.ml
+++ src/tool/cmd_concrete.ml
@@ -8,13 +8,9 @@ let info = Cmd.info "concrete" ~exits
 let term =
   let open Term.Syntax in
   (* les + servent à concatenner les argments/options de la cmd *)
-  let+ () = setup_log 
-  and+ source_file = source_file 
-  and+ seed = seed in
-  (match seed with
-    | Some n -> Random.init n
-    | None -> Random.self_init());
-    (* c ici que le moteur d'owi execute les .wat *)
+  let+ () = setup_log and+ source_file = source_file and+ seed = seed in
+  (match seed with Some n -> Random.init n | None -> Random.self_init ());
+  (* c ici que le moteur d'owi execute les .wat *)
   Ono.Concrete_driver.run ~source_file |> function
   | Ok () -> Ok ()
   | Error e -> Error (`Msg (Kdo.R.err_to_string e))
