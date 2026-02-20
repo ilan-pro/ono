d2cebe3bd746f9b57743672d308a37d983dc4a88
justussa <kevin.justus-sathananthan@etu.u-paris.fr>
2026-02-20 15:20:24 +0100

    affichage terminal print_grid

--- src/lib/concrete_ono_module.ml
+++ src/lib/concrete_ono_module.ml
@@ -11,6 +11,18 @@ let print_i64 (n : Kdo.Concrete.I64.t) : (unit, _) Result.t =
 let random_i32 (n : Kdo.Concrete.I32.t) : (Kdo.Concrete.I32.t, _) Result.t =
   Ok (Kdo.Concrete.I32.of_int (Random.int (Kdo.Concrete.I32.to_int n)))
 
+let buffer = Buffer.create(4096)
+let cell_print (x: Kdo.Concrete.I32.t) : (unit, _) Result.t =
+  if x <> (Kdo.Concrete.I32.of_int 0) then
+    Buffer.add_string buffer "🦊"
+  else
+    Buffer.add_string buffer " ";
+  Ok()
+
+  
+
+  
+
 let m =
   let open Kdo.Concrete.Extern_func in
   let open Kdo.Concrete.Extern_func.Syntax in
@@ -19,6 +31,8 @@ let m =
       ("print_i32", Extern_func (i32 ^->. unit, print_i32));
       ("print_i64", Extern_func (i64 ^->. unit, print_i64));
       ("random_i32", Extern_func (i32 ^->. i32, random_i32));
+      ("cell_print", Extern_func (i32 ^->. unit, cell_print));
+
     ]
   in
   {
